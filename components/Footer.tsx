3d922815f5694fadfebfafb98be3d600d77e1fe9
Demoaccount <noorali.momtaz98@gmail.com>
2025-02-11 02:01:46 +0530

    changes in ui

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image src="Travalix-bg.svg" alt="logo" width={104} height={29} />
+            <Image src="travalix-bg.svg" alt="logo" width={104} height={29} />
           </Link>
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
