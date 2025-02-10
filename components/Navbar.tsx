b66e58038bd1b55f7db2713ec78f28508cc6a256
Demoaccount <noorali.momtaz98@gmail.com>
2025-02-11 01:56:33 +0530

    changes in ui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+        <Image src="/travalix-bg.svg" alt="logo" width={104} height={39} />
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
